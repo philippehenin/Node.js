19c132a2eb53f3d15bcb847ce5206dada22c915f
philippehenin <philippehenin@gmail.com>
2016-01-07 17:20:18 +0100

    phe2.js  good for demo...

--- phe2.js
+++ phe2.js
@@ -2,79 +2,28 @@
 
 var kafka = require('..');
 var couchbase = require("couchbase");
-var uuid = require("uuid");
 var config = require("./config");
-
 var argv = require('optimist').argv;
-
 var HighLevelConsumer = kafka.HighLevelConsumer;
 var Client = kafka.Client;
-
-
-
-
-
-//console.log("kafka");
-//console.log(config.kafka.topic);
-//console.log(config.kafka.server);
-//console.log("couchbase")
-//console.log(config.couchbase.bucket);
-//console.log(config.couchbase.server);
-
 var topic = argv.topic || config.kafka.topic || 'topic1';
-
 var client = new Client(config.kafka.server);
 var topics = [ { topic: topic }];
 var options = { autoCommit: true, fetchMaxWaitMs: 1000, fetchMaxBytes: 1024*1024 };
 var consumer = new HighLevelConsumer(client, topics, options);
-
-
-
-
 var bucket = (new couchbase.Cluster(config.couchbase.server)).openBucket(config.couchbase.bucket);
 
-var jsonData = {
-    id: uuid.v4(),
-   username: "philippehenin",
-}
-
-
-
-
-
-
-
 consumer.on('message', function (message) {
-    console.log(message);
-
-// A supprimer quand pascql a corrigé
-    jsonData = {
-        key: message.value.key,
-        data: message.value
-    }
-
-
     var buffer = JSON.parse(message.value)
-    
-    console.log(" test buffer -> "+ buffer )
-
-    console.log(" test buffer.key  -> "+ buffer.key )
-    
-    // A mettre en place quand Pascal maitrisera son json...
+    //console.log(" test buffer -> "+ buffer )
+    //console.log(" test buffer.key  -> "+ buffer.key )
      bucket.insert(buffer.key, buffer, function(error, result) {
-    // bucket.insert(uuid.v4(), jsonData, function(error, result) {
         if (error) {
           console.log('Failed to save to Couchbase', error);
         } else {
-          console.log('Saved to Couchbase!');
+          console.log(buffer.key);
         }
     });
-
-
-
-
-
-
 });
 
 consumer.on('error', function (err) {
